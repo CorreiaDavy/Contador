fd902e41db29407d04b42e9bab764fdd6459ac27
@Davycorreia <franciscodavydossantoscorreia@gmail.com>
2024-08-17 22:15:03 -0300

    Funções
    Foram adicionadas funções de incrementar e decrementar, como também de reiniciar a contagem.

---
+++ js/script.js
@@ -0,0 +1,54 @@
+
+ 
+/* função para diminuir o valor contador*/
+function botao_menos(){
+    const menuButao = document.querySelector("#botao_decre").addEventListener("click", function(){
+        const input_menos = document.getElementById("numero_contador").value--
+
+        
+    })
+
+
+   
+}
+
+/* função para aumentar o valor contador*/
+function botao_mais(){
+    const menuBotao_mais = document.querySelector("#bot_menos_decre").addEventListener("click", function(){
+      const input_mais = document.getElementById("numero_contador").value++
+      
+
+    })
+}
+
+/* Botão de reiniciar contagem*/
+function input_zero(){
+
+    const botao_zerar = document.querySelector("#botaozerar").addEventListener("click", function(){
+
+        const valorInput = document.getElementById("numero_contador")
+        valorInput.value = 0
+
+    })
+    
+    
+
+    
+}
+
+
+
+input_zero()
+botao_menos()
+botao_mais()
+
+
+   
+  
+
+       
+    
+
+
+
+
